ecf156421e3f32518053e0dcfd51ab0ddb16e284
Ariel <arielkhoutorsky1@gmail.com>
2024-04-18 00:13:15 -0400

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+
+import { cn } from "@/lib/utils"
+
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans"
+})
+
+export const metadata: Metadata = {
+    title: "Doctor Connect",
+    description: "Contact a doctor today!",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+                <ThemeProvider>
+                    {children}
+                </ThemeProvider>
+
+            </body>
+        </html>
+    );
+}
